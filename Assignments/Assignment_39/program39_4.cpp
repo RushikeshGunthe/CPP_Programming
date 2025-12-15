d70f122781116f50e467958c81399231e05b8cb5
Rushikesh Gunthe <guntherushi111@gmail.com>
2025-12-15 17:03:11 +0530

    Add Assignment_39 Q4

---
+++ Assignments/Assignment_39/program39_4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Pattern 
+{
+
+    public:
+        
+        char cAlpha = 'A'; 
+
+        // Recursive function inside class
+        void Display(int iNo) 
+        {
+
+            // Base condition
+            if (iNo == 0)
+            {
+                return;
+            }
+
+            cout << cAlpha <<"\t";
+            cAlpha++;
+
+            // Recursive call
+            Display(iNo - 1);
+        }
+};
+
+int main() {
+    Pattern pobj;   // Object creation
+    pobj.Display(6); // Method call
+    return 0;
+}
