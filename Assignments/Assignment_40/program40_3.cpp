ebaa531698be5786a52486c0244ef7f07249f9a4
Rushikesh Gunthe <guntherushi111@gmail.com>
2025-12-15 21:34:53 +0530

    Add Assignment_40 Q3

---
+++ Assignments/Assignment_40/program40_3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Pattern 
+{
+
+    public:
+
+        // Recursive function inside class
+        void Display(int iNo) 
+        {
+
+            // Base condition
+            if (iNo == 0)
+            {
+                return;
+            }
+
+            cout << iNo <<"\t";
+
+            // Recursive call
+            Display(iNo - 1);
+        }
+};
+
+int main() 
+{
+    int iValue = 0;
+
+    cout << "Enter how many numbers you want to display in reverse: \n";
+    cin >> iValue;
+
+    Pattern pobj;   // Object creation
+
+    pobj.Display(iValue); // Method call
+    
+    return 0;
+}
