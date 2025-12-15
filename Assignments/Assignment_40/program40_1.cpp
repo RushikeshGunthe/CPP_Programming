a229b32f2a008f08a5b9717b2c72e77f0a8485f5
Rushikesh Gunthe <guntherushi111@gmail.com>
2025-12-15 21:34:34 +0530

    Add Assignment_40 Q1

---
+++ Assignments/Assignment_40/program40_1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Pattern
+{
+
+    public:
+
+        // Recursive function inside class
+        void Display(int iNo) 
+        {
+
+            // Base condition
+            if (iNo == 0)
+            {
+                return;
+            }
+
+            // Print one star
+            cout << "* \t";
+
+            // Recursive call
+            Display(iNo - 1);
+        }
+};
+
+int main() 
+{
+    int iValue = 0;
+
+    cout << "Enter how many * you want to display: \n";
+    cin >> iValue;
+
+    Pattern pobj;   // Object creation
+
+    pobj.Display(iValue); // Method call
+    
+    return 0;
+}
